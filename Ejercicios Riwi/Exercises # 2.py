c4635f2b63753e5233d352d5414e7e8e6dca8a49
david <david.990320@gmail.com>
2025-04-24 00:31:57 -0500

    proceso riwi

---
+++ Ejercicios Riwi/Exercises # 2.py
@@ -0,0 +1,15 @@
+#Hola Mundo personalizado
+#Pide al usuario su nombre y muestra un saludo personalizado en pantalla.
+
+def hola():
+    
+    name = print(input("Enter you name: "))
+    
+    if name :
+        
+        print(f"Hello {name} ! is a pleasure know you.")
+    else:
+        print("Enter corretly you name") 
+print(type) 
+
+hola()           
